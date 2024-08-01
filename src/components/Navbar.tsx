b51e7677b1fd9929830238e5c566e0ae801d8961
Aborisade <bobleeswaggs123@gmail.com>
2024-08-01 14:56:08 +0200

    Added the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,22 @@
 import logo from "../assets/drive-48.svg";
 import {
-  MagnifyingGlassIcon,
   Cog6ToothIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/24/outline";
-import { Squares2X2Icon } from "@heroicons/react/24/solid";
+import { Squares2X2Icon,
+  MagnifyingGlassIcon
+ } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between">
       <a href="/" className="flex items-center">
         <div>
           <img src={logo} alt="drive_logo" className="" />
         </div>
-        <h1 className="text-2xl">Drive</h1>
+        <h1 className="text-[28px]">Drive</h1>
       </a>
-      <div className="relative flex items-center text-gray-400 focus-within:text-gray-600 ">
+      <div className="relative flex items-center text-black focus-within:text-black ">
         <MagnifyingGlassIcon className="absolute h-5 w-5 ml-3 pointer-events-none" />
         <input
           type="search"
@@ -23,14 +24,14 @@ const Navbar = () => {
           placeholder="Search in Drive"
           autoComplete="off"
           aria-label="Search talk"
-          className="pr-3 pl-12 py-2 placeholder-gray-500 rounded-3xl
-            border-none ring-2 ring-gray-200 focus:ring-gray-500 focus:ring-2 focus:outline-none"
+          className="pr-3 pl-12 py-2 w-[600px] h-12 placeholder-gray-500 bg-[#c8dde0] focus:bg-white rounded-3xl
+            border-none focus:shadow-lg ring-gray-200 focus:ring-gray-500 focus:outline-none"
         />
       </div>
-      <div>
-        <Cog6ToothIcon className="w-6 h-6" />
-        <QuestionMarkCircleIcon className="w-6 h-6" />
-        <Squares2X2Icon className="w-6 h-6"/>
+      <div className="flex ">
+        <Cog6ToothIcon className="w-7 h-7" />
+        <QuestionMarkCircleIcon className="w-7 h-7" />
+        <Squares2X2Icon className="w-7 h-7"/>
       </div>
     </div>
   );
