1ac43abe0ac3bf31119aeb0b6f568adcd8473b0c
Aborisade <bobleeswaggs123@gmail.com>
2024-08-13 00:41:06 +0200

    a little change to the NewUpload component

--- src/components/layout/modal/NewUpload.tsx
+++ src/components/layout/modal/NewUpload.tsx
@@ -1,6 +1,10 @@
 const NewUpload = () => {
   return (
-    <div>this is a new component, which i created yesterday</div>
+    <div>
+      <div>this is a new component, which i created yesterday</div>
+      <div>Have  not really have time to make any significant changes</div>
+    </div>
+
   )
 }
 
