9630839274c8c379c9dd3d25cc7f574fa9ae7e44
Aborisade <bobleeswaggs123@gmail.com>
2024-08-14 02:14:06 +0200

    worked on the upload modal and started firebase

--- src/components/layout/modal/NewUpload.tsx
+++ src/components/layout/modal/NewUpload.tsx
@@ -1,15 +1,34 @@
-const NewUpload = () => {
+import { FolderArrowDownIcon } from "@heroicons/react/24/outline";
+import { FolderIcon } from "@heroicons/react/24/outline";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+
+const NewUpload: React.FC<{ isVisible: boolean; closeModal: () => void }> = ({ isVisible, closeModal }) => {
   return (
-    <div className="bg-slate-300 absolute">
-      <div>
-        <button>
-          New Folder
+    <div
+      className={`fixed bg-white w-80 h-52 z-50 transition-opacity duration-300 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        }`}
+    >
+      <div className="px-2 py-1 my-2 hover:bg-[#cddee0]">
+        <button className="flex gap-4">
+          <FolderIcon className="w-6 h-6"/>
+          <h1>New Folder</h1>
         </button>
       </div>
-      <div>
-        <button onClick={() =>console.log("create file")}>File Upload</button>
-        <button onClick={() => console.log("create folder")}>Folder Upload</button>
+      <hr />
+      <div className="px-2 py-1 my-2 hover:bg-[#cddee0]">
+        <button className="flex gap-4" onClick={() => console.log("create file")}>
+          <FolderArrowDownIcon className="w-6 h-6"/>
+          <h1>File Upload</h1>
+        </button>
       </div>
+      <div className="px-2 py-1 my-2 hover:bg-[#cddee0]">
+        <button className="flex gap-4 " onClick={() => console.log("create folder")}>
+          <FolderArrowDownIcon className="w-6 h-6"/>
+          <h1>Folder Upload</h1>
+        </button>
+      </div>
+      <hr />
+      <XMarkIcon className="w-6 h-6 absolute top-1 right-1" onClick={closeModal} />
     </div>
 
   )
---
+++ src/config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdPTbur-2-laNykmPf6NYH13Go3SDOl48",
+  authDomain: "drive-clone-4c5ac.firebaseapp.com",
+  projectId: "drive-clone-4c5ac",
+  storageBucket: "drive-clone-4c5ac.appspot.com",
+  messagingSenderId: "1058247160985",
+  appId: "1:1058247160985:web:3bce8f634d121cb8fab408",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
