f43e973cd276361aa48d5e9fac4a231700decddf
Aborisade <bobleeswaggs123@gmail.com>
2024-08-29 22:03:33 +0100

    still on the uploadfile fuctionality

--- src/components/layout/modal/Upload.tsx
+++ src/components/layout/modal/Upload.tsx
@@ -5,19 +5,26 @@ import {v4 as uuidv4} from "uuid"
 
 const Upload = () => {
   const [fileUpload, setFileUpload] = useState<File | null>(null);// this is to save the file in a state before sending it to firebase
+  const [fileList, setFileList] = useState([])
+  const fileList = ref(storage, "file 1")
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if(event.target.files) {
       setFileUpload(event.target.files[0])
     }
   }
+  const uploadFile = async () => {
+    if(fileUpload == null) return;
+    const fileRef = ref(storage, `file 1/${fileUpload.name + uuidv4()}`);
+    try {
+      await uploadBytes(fileRef, fileUpload);
+      alert("file uploaded successfully")
+    } catch (error) {
+      alert("omo this thing no upload ooo")
+    }
+  }
   // useEffect(() => {
   //   console.log(fileUpload)
   // }, [fileUpload])
-  const uploadFile = () => {
-    if(fileUpload == null) return;
-    const fileRef = ref(storage, `file 1/${fileUpload.name + uuidv4()}`);
-    uploadBytes(fileRef, fileUpload)
-  }
   return (
     <div>
         <input type="file" onChange={handleFileChange} />
